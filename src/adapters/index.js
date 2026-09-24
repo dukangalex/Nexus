fa6f6c6d69087a80b58eb192f964df9d30551e89
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-24 05:36:54 -0700

    refactor: make kernel adapters first-class contracts

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,1 +1,32 @@
-import {Kernels} from "../core/model.js"; export const adapters={ [Kernels.MIHOMO]:{chainMechanism:"dialer-proxy"}, [Kernels.SING_BOX]:{chainMechanism:"detour"}, [Kernels.XRAY]:{chainMechanism:"streamSettings.sockopt.dialerProxy"} }; export function adapterFor(kernel){return adapters[kernel]||null;}
+import { Kernels } from "../core/model.js";
+import { createKernelAdapter, AdapterCapabilities } from "./contract.js";
+import { compileMihomoChain } from "./mihomo/compiler.js";
+import { compileSingBoxChain } from "./sing-box/compiler.js";
+import { compileXrayChain } from "./xray/compiler.js";
+
+export { AdapterCapabilities, createKernelAdapter };
+
+export const adapters = Object.freeze({
+  [Kernels.MIHOMO]: createKernelAdapter({
+    kernel: Kernels.MIHOMO,
+    capabilities: [AdapterCapabilities.CHAIN_COMPILE],
+    chainMechanism: "dialer-proxy",
+    compileChain: compileMihomoChain,
+  }),
+  [Kernels.SING_BOX]: createKernelAdapter({
+    kernel: Kernels.SING_BOX,
+    capabilities: [AdapterCapabilities.CHAIN_COMPILE],
+    chainMechanism: "detour",
+    compileChain: compileSingBoxChain,
+  }),
+  [Kernels.XRAY]: createKernelAdapter({
+    kernel: Kernels.XRAY,
+    capabilities: [AdapterCapabilities.CHAIN_COMPILE],
+    chainMechanism: "streamSettings.sockopt.dialerProxy",
+    compileChain: compileXrayChain,
+  }),
+});
+
+export function adapterFor(kernel) {
+  return adapters[kernel] || null;
+}
