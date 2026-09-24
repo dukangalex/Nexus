01b995ce61df1d387893cb6c059e5ab86be7eba3
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-24 07:01:07 -0700

    feat: export unified config model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export * from "./core/model.js"; export * from "./core/sniffer.js"; export * from "./core/chain.js"; export * from "./core/chain-compiler.js"; export * from "./core/config.js"; export * from "./core/controller.js"; export * from "./core/network.js"; export * from "./core/security.js"; export * from "./core/bypass.js"; export * from "./core/self-healing.js"; export * from "./core/dns.js"; export * from "./core/lifecycle.js"; export * from "./adapters/index.js";
- export * from "./platform/index.js";
+export * from "./platform/index.js";
 export * from "./core/subscription.js";
 export * from "./core/region.js";
 export * from "./core/import-pipeline.js";
 export * from "./core/kernel-capabilities.js";
+export * from "./core/unified-config.js";
