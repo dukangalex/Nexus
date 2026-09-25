d7b05f2377803ac5c60f78c4b1b837e7d02b6305
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-25 00:21:37 -0700

    feat: export platform bridge

--- src/platform/index.js
+++ src/platform/index.js
@@ -2,4 +2,7 @@ export {
   PlatformId,
   PlatformCapabilities,
   createPlatformContract,
+  getPlatformCapabilityRequirements,
 } from "./contract.js";
+
+export { createPlatformBridge } from "./bridge.js";
