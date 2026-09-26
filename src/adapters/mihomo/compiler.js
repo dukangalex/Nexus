08f1a2e806239c5e6f308d95478fcac64bff2bba
dukangalex <128732965+dukangalex@users.noreply.github.com>
2026-09-26 00:19:45 -0700

    security: compile encrypted DNS natively per kernel

--- src/adapters/mihomo/compiler.js
+++ src/adapters/mihomo/compiler.js
@@ -1,6 +1,7 @@
 import { validateChain } from "../../core/chain.js";
 import { Kernels } from "../../core/model.js";
 import { compileRoutingPolicy } from "../../core/routing-compiler.js";
+import { compileDnsConfig } from "../../core/dns.js";
 
 function clone(value) { return value && typeof value === "object" ? structuredClone(value) : value; }
 function requireProxy(config, id) {
@@ -59,7 +60,7 @@ function applySecurityRules(output, config) {
 export function compileMihomoConfig(config) {
   const output = { proxies: (config.nodes || []).map(compileNode) };
   if (Array.isArray(config.groups) && config.groups.length) output["proxy-groups"] = clone(config.groups);
-  if (config.dns && Object.keys(config.dns).length) output.dns = clone(config.dns);
+  output.dns = compileDnsConfig(config, Kernels.MIHOMO);
   if (config.routing && Object.keys(config.routing).length) {
     output.rules = compileRoutingPolicy(config.routing, Kernels.MIHOMO);
     applySecurityRules(output, config);
